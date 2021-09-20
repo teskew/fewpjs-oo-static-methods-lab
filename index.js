5673a7e6fe24416e7a742819e46eb06a0c71bc49
github username <github email address>
2021-09-20 11:36:46 -0500

    commit tree c8e8e273bbd9d0e052ccefd6008eef324104f51e

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+static capitalize(str){
+  
+return str.split("")[0].toUpperCase() + str.slice(1);
+}
+ static sanitize(str){
+
+  return str.replace(/[^A-Za-z0-9-" "']+/g, '');
+ }
+static titleize(string){
+  let words = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+  let strArr = string.split(" ");
+  let finalString = [];
+
+  strArr.map(function(word){
+    if (words.includes(word) === false){
+      finalString.push(Formatter.capitalize(word));
+    
+    }else{
+      finalString.push(word)
+    }
+  })
+
+ return Formatter.capitalize(finalString.join(" "));
+};
+
 }
